a939296a84d6d7c4a8f240c350f1913c496bba8b
ajangeunajang <ajangeunajang@gmail.com>
2025-12-30 17:30:57 +0900

    Feat: backdrop blur projectcard

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -43,7 +43,7 @@ export default function ProjectCard({ project, type = 'project' }) {
         }
       }}
     >
-      <div className="absolute w-full h-full bg-black/50 group-hover:bg-black/0 transition-bg duration-200 leading-tight">
+      <div className="filter hover:backdrop-blur-sm absolute w-full h-full bg-black/50 group-hover:bg-black/0 transition-bg duration-200 leading-tight">
       <h3 className="scale-x-[0.6] group-hover:opacity-0 transition-opacity duration-300 mt-1">
           {project.host}
         </h3>
