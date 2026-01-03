62dfa4505c3888c29abdca6c581b490402b9114f
ajangeunajang <ajangeunajang@gmail.com>
2026-01-03 12:44:03 +0900

    Fix: about !no-underline

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -52,7 +52,7 @@ export default function About() {
                   <td className="w-12 lg:pr-32 pr-20 align-top">
                     {contact.label}
                   </td>
-                  <td className="deleteunderline">{contact.value}</td>
+                  <td className="deleteunderline !no-underline">{contact.value}</td>
                 </tr>
               ))}
             </tbody>
