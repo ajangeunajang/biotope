5d99fba4c3481f0fd07487cf7cfbacd0ac2195f7
ajangeunajang <ajangeunajang@gmail.com>
2026-01-03 12:07:19 +0900

    Fix: about 밑줄제거

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -52,7 +52,7 @@ export default function About() {
                   <td className="w-12 lg:pr-32 pr-20 align-top">
                     {contact.label}
                   </td>
-                  <td className="">{contact.value}</td>
+                  <td className="deleteunderline">{contact.value}</td>
                 </tr>
               ))}
             </tbody>
