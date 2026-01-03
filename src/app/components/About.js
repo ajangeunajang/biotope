e6044fb9dcd04f36bea5739868c0380304e9b67c
ajangeunajang <ajangeunajang@gmail.com>
2026-01-03 13:14:34 +0900

    Del: 밑줄제거 tailwind 삭제

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -26,7 +26,7 @@ export default function About() {
   if (loading) {
     return (
       <div className="fixed top-0 right-0 w-2/3 h-full flex items-center justify-center">
-        <div className="text-white text-xl flex items-center gap-2">
+        <div className="text-white text-lg lg:text-3xl flex items-center gap-2">
           <div className="w-4 h-4 border-2 border-white border-t-transparent animate-spin"></div>
           <span>Loading...</span>
         </div>
@@ -45,22 +45,15 @@ export default function About() {
   return (
       <div className="fixed top-0 right-0 w-2/3 h-3/4 m-4 lg:pl-12 sm:pl-8 pl-6 pr-4 lg:py-10 py-4 text-white lg:flex items-start overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
         <div className="flex-1 w-full pb-10 lg:h-full">
-          <table className="lg:fixed origin-left scale-x-[0.6] w-3/2 text-xl lg:text-3xl">
+          <table className="lg:fixed origin-left scale-x-[0.6] w-3/2 text-lg lg:text-3xl">
             <tbody>
               {aboutData.contact?.map((contact, index) => (
                 <tr key={index} className="leading-tight overflow-x-hidden">
                   <td className="w-12 lg:pr-32 pr-20 align-top">
                     {contact.label}
                   </td>
-                  <td className="deleteunderline !no-underline !decoration-transparent">
-                    <span style={{
-                      textDecoration: 'none',
-                      WebkitTextDecoration: 'none',
-                      textDecorationLine: 'none',
-                      WebkitTextDecorationLine: 'none',
-                      color: 'inherit',
-                      pointerEvents: 'none'
-                    }}>
+                  <td>
+                    <span>
                       {contact.value}
                     </span>
                   </td>
@@ -70,10 +63,10 @@ export default function About() {
           </table>
         </div>
         <div className="flex-1 h-auto pb-10 sm:pb-0 overflow-x-hidden">
-          <pre className="whitespace-pre-wrap break-words origin-left scale-x-[0.6] w-3/2 text-xl lg:text-3xl mb-20">
+          <pre className="whitespace-pre-wrap break-words origin-left scale-x-[0.6] w-3/2 text-lg lg:text-3xl mb-20">
             {aboutData.descriptionKo}
           </pre>
-          <pre className="whitespace-pre-wrap break-words origin-left scale-x-[0.6] w-3/2 text-xl lg:text-3xl mb-20">
+          <pre className="whitespace-pre-wrap break-words origin-left scale-x-[0.6] w-3/2 text-lg lg:text-3xl mb-20">
             {aboutData.descriptionEn}
           </pre>
         </div>
